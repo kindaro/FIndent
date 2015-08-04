1cbc576f3bb230f89cdda910c40ebc6b026aebad
Ignat Insarov <kindaro@gmail.com>
2015-08-05 02:27:34 +0300

    Initial commit.

---
+++ gulpfile.coffee
@@ -0,0 +1,51 @@
+gulp = require ('gulp')
+coffee = require ('gulp-coffee')
+jade = require ('gulp-jade')
+stylus = require ('gulp-stylus')
+yaml = require ('gulp-yaml')
+sass = require ('gulp-sass')
+minifyHTML = require ('gulp-minify-html')
+
+gulp.task 'default', ['coffee', 'jade', 'styl', 'yaml']
+
+gulp.task 'watch',
+    ->
+        gulp.watch './src/**', ['default']
+
+gulp.task 'gulpfile',
+    ->
+        gulp.src './gulpfile.coffee'
+        .pipe coffee {bare: true}
+        .pipe gulp.dest './'
+
+gulp.task 'jade',
+    ->
+        gulp.src './src/**/*.jade'
+        .pipe jade {pretty: true}
+        .pipe minifyHTML { }
+        .pipe gulp.dest './build'
+
+gulp.task 'coffee',
+    ->
+        gulp.src './src/**/*.coffee'
+        .pipe coffee {bare: true}
+        .pipe gulp.dest './build'
+
+gulp.task 'styl',
+    ->
+        gulp.src './src/**/*.styl'
+        .pipe stylus {}
+        .pipe gulp.dest './build'
+
+gulp.task 'yaml',
+    ->
+        gulp.src './src/**/*.yml'
+        .pipe yaml { space: 4 }
+        .pipe gulp.dest './build'
+
+gulp.task 'inuit',
+    ->
+        gulp.src './inuit.css-web-template/css/style.scss'
+        .pipe sass {}
+            .on 'error', sass.logError
+        .pipe gulp.dest './build'
---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var coffee, gulp, jade, minifyHTML, sass, stylus, yaml;
+
+gulp = require('gulp');
+
+coffee = require('gulp-coffee');
+
+jade = require('gulp-jade');
+
+stylus = require('gulp-stylus');
+
+yaml = require('gulp-yaml');
+
+sass = require('gulp-sass');
+
+minifyHTML = require('gulp-minify-html');
+
+gulp.task('default', ['coffee', 'jade', 'styl', 'yaml']);
+
+gulp.task('watch', function() {
+  return gulp.watch('./src/**', ['default']);
+});
+
+gulp.task('gulpfile', function() {
+  return gulp.src('./gulpfile.coffee').pipe(coffee({
+    bare: true
+  })).pipe(gulp.dest('./'));
+});
+
+gulp.task('jade', function() {
+  return gulp.src('./src/**/*.jade').pipe(jade({
+    pretty: true
+  })).pipe(minifyHTML({})).pipe(gulp.dest('./build'));
+});
+
+gulp.task('coffee', function() {
+  return gulp.src('./src/**/*.coffee').pipe(coffee({
+    bare: true
+  })).pipe(gulp.dest('./build'));
+});
+
+gulp.task('styl', function() {
+  return gulp.src('./src/**/*.styl').pipe(stylus({})).pipe(gulp.dest('./build'));
+});
+
+gulp.task('yaml', function() {
+  return gulp.src('./src/**/*.yml').pipe(yaml({
+    space: 4
+  })).pipe(gulp.dest('./build'));
+});
+
+gulp.task('inuit', function() {
+  return gulp.src('./inuit.css-web-template/css/style.scss').pipe(sass({})).on('error', sass.logError).pipe(gulp.dest('./build'));
+});
