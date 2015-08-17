ab3724ff3c5cda0a02bca549e3d7cac4ba4b2d14
Ignat Insarov <kindaro@gmail.com>
2015-08-17 15:45:40 +0300

    Re-initialize with just one command line.
    The script would re-initialize git repo, set npm package metadata, commit and publish on GitHub.

---
+++ initialize.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+
+# Perform destructive re-initialization of the project.
+# -----------------------------------------------------
+
+# Warning: DESTRUCTIVE
+
+
+# Effectful execution functions.
+# -------------------------------------
+
+logic ()
+{
+    name="$1"
+    description="$2"
+    username="$3"
+    subId="`date +%s`"
+
+    mkdir "$subId"
+
+    # Reset Git repo.
+    mv ".git" "${subId}/.git.archive"
+    git init
+
+    # Set npm project name, project description, associated variables.
+    # (The latter implies "repository", "bugs" and "homepage")
+    sed -i".archive" -r '
+        s/"name": "initial"/"name": "'"$name"'"/
+        s/"description": "[^"]*"/"description": "'"$description"'"/
+        s/(.*)initial(.*)/\1'"$name"'\2/
+        ' 'package.json'
+    mv 'package.json.archive' "$subId"
+
+    # Write a readme.
+    echo "$description" > README.md
+
+    # Commit changes.
+    git add package.json
+    git add README.md
+    git add gulpfile.{js,coffee}
+    git add src
+    git commit -m "Automatic initial commit. All things set up."
+    
+    # Synchronize.
+    curl -u "$username" https://api.github.com/user/repos -d '{"name":"'"$name"'"}' >/dev/null
+    git remote add origin "git@github.com:${username}/${name}.git"
+    git push --set-upstream origin master
+
+    # Clean up.
+    rm -rf "$subId"
+
+    return 0
+}
+
+# User interfacing.
+# -----------------
+
+if test $# -eq 0
+then # Interactive.
+
+    echo "Re-initialization will destroy git history in here. Sure to proceed ?"
+
+    select yn in "Yes" "No"
+    do
+        case $yn in 
+            (Yes)
+                echo "How would you like to name your new project ?"
+                read name
+                echo "How would you like to describe your new project ?"
+                read description
+                echo "How would you like to present yourself? (Default: ${USER})"
+                read username
+
+                if not test "$username"
+                then
+                    username="$USER"
+                fi
+
+                logic "$name" "$description" "$username"
+                exit
+                ;;
+            (No)
+                exit
+                ;;
+        esac
+    done
+else # Batch.
+    name="$1"
+    description="$2"
+    username="$3"
+    logic "$name" "$description" "$username"
+fi
+
+exit # In any way.
